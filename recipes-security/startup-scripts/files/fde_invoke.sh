7f6dde3a0c76429778cf08d7b2e549bcc1c46dcd
Divya Theja <divyathe@codeaurora.org>
2018-08-01 16:13:39 +0530

    fde_invoke:add selinux context labelling support for /data
    label the /data appropriately with selinux context
    once the userdata is encrypted and mounted at /data.
    
    Change-Id: Ib6228d2c0d4ffd6fa16fe589fbde51de11090e65

--- recipes-security/startup-scripts/files/fde_invoke.sh
+++ recipes-security/startup-scripts/files/fde_invoke.sh
@@ -6,3 +6,12 @@
 
 set -e
 /usr/bin/hwfde_service
+if [ $? -ne 0 ]; then
+    exit
+fi
+
+datamountret=`mount | grep /data | wc -l`
+
+if [ $datamountret == "1" ]; then
+    /sbin/restorecon -R /data
+fi
