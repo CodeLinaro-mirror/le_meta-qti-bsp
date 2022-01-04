0f8761b5778bda241da5c822bbf558c88ae1f96b
ziqic <quic_ziqic_01@quicinc.com>
2022-01-04 10:48:17 +0800

    Gnss services: stop gnss services to solve suspend failed
    Stop gnss services to solve suspend failed in LV3.2.
    
    Change-Id: I5ed1a613e4d2dd9d3d0f18550664819fda468f30
    Signed-off-by: ziqic <quic_ziqic_01@quicinc.com>

--- meta-qti-extra/recipes-core/systemd/systemd/qti_sleep.sh
+++ meta-qti-extra/recipes-core/systemd/systemd/qti_sleep.sh
@@ -30,6 +30,8 @@ case $1/$2 in
   pre/*)
     echo "Entering into $2..."
 
+    systemctl stop loc_launcher.service
+    systemctl stop location_hal_daemon.service
     systemctl stop audiod.service
     if [ $2 == "hibernate" ]; then
         echo 0 > /sys/kernel/boot_adsp/boot
@@ -64,6 +66,8 @@ case $1/$2 in
     if [ $2 == "hibernate" ]; then
         echo 1 > /sys/kernel/boot_adsp/boot
     fi
+    systemctl restart loc_launcher.service
+    systemctl restart location_hal_daemon.service
     systemctl restart audiod.service
     ;;
 esac
