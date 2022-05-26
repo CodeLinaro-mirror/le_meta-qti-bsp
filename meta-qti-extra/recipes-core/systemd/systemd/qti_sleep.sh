57585e1cc7dbeda25b02e112ec5e91fd9243842e
Xiaoning Ma <quic_mxiaonin@quicinc.com>
2022-05-26 17:45:38 +0800

    meta-qti-bsp: Unload/reload WLAN driver for LPM
    Unload/reload wlan module during system LPM.
    
    Change-Id: I1127cb0e11c16fb8d6f154e4ba2efd1679923d5e

--- meta-qti-extra/recipes-core/systemd/systemd/qti_sleep.sh
+++ meta-qti-extra/recipes-core/systemd/systemd/qti_sleep.sh
@@ -25,6 +25,41 @@
 # WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
 # OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
 # IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Changes from Qualcomm Innovation Center are provided under the following license:
+#
+# Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted (subject to the limitations in the
+# disclaimer below) provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above
+#      copyright notice, this list of conditions and the following
+#      disclaimer in the documentation and/or other materials provided
+#      with the distribution.
+#
+#    * Neither the name of Qualcomm Innovation Center, Inc. nor the names of its
+#      contributors may be used to endorse or promote products derived
+#      from this software without specific prior written permission.
+#
+# NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE
+# GRANTED BY THIS LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT
+# HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED
+# WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+# MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+# OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+# IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 
 usb_mode_file="/var/usb/usb_mode.txt"
 usb_dev_path="/sys/devices/platform/soc"
@@ -39,6 +74,9 @@ case $1/$2 in
     if [ $2 == "hibernate" ]; then
         echo 0 > /sys/kernel/boot_adsp/boot
     fi
+
+    # unload WLAN driver
+    systemctl stop init_qti_wlan_auto.service
 
     # disable BT as hsuart could block suspend
     systemctl stop synergy.service
@@ -77,5 +115,8 @@ case $1/$2 in
     systemctl restart loc_launcher.service
     systemctl restart location_hal_daemon.service
     systemctl restart audiod.service
+
+    #load WLAN
+    systemctl restart init_qti_wlan_auto.service
     ;;
 esac
