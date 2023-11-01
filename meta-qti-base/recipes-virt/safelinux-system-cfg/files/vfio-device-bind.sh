370c36a17d8bbf3685e1eaf39b1c86ee3258e24e
kaixzhou <quic_kaixzhou@quicinc.com>
2023-11-01 14:20:11 +0530

    meta-qti-bsp: Add qti-usermode-camera
    Add vfio_titan to vfio-device-bind.sh
    
    Change-Id: I3fd6f927261c34280226101e5d8d1a8212f7dac4

--- meta-qti-base/recipes-virt/safelinux-system-cfg/files/vfio-device-bind.sh
+++ meta-qti-base/recipes-virt/safelinux-system-cfg/files/vfio-device-bind.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
-DEVS="17c23000.vfio_timer ae00000.vfio_dpu_00 aa00000.vfio_vidc 408000.umd_pil soc:vfio_vidc_non_secure_pixel_cb"
+DEVS="17c23000.vfio_timer ae00000.vfio_dpu_00 aa00000.vfio_vidc 408000.umd_pil soc:vfio_vidc_non_secure_pixel_cb ac00000.vfio_titan_base"
 
 for DEV in $DEVS; do
     echo "vfio-platform" > /sys/bus/platform/devices/$DEV/driver_override
