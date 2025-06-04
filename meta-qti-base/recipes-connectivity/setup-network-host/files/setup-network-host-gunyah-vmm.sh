7f507fad82d37cd9d815bc36d31021f8eb473c7b
Uppalamarthi Sowmya <quic_usowmya@quicinc.com>
2025-06-04 16:52:05 +0530

    meta-qti-bsp: Add buffer settings
    Add buffer settings and install rps.
    
    Change-Id: Ibdbb1cc9a2387d13cf7018dcb4903981c6a5f5be
    Signed-off-by: Uppalamarthi Sowmya <quic_usowmya@quicinc.com>

--- meta-qti-base/recipes-connectivity/setup-network-host/files/setup-network-host-gunyah-vmm.sh
+++ meta-qti-base/recipes-connectivity/setup-network-host/files/setup-network-host-gunyah-vmm.sh
@@ -2,7 +2,32 @@
 # Copyright (c) 2024 Qualcomm Innovation Center, Inc. All rights reserved.
 # SPDX-License-Identifier: BSD-3-Clause-Clear
 
-# parameters: 
+
+perf_set_if()
+{
+sysctl -w net.core.rmem_max=33554432
+sysctl -w net.core.wmem_max=262144
+sysctl -w net.ipv4.tcp_timestamps=0
+echo 16777216 > /proc/sys/net/core/rmem_default
+echo 16777216 > /proc/sys/net/core/wmem_default
+echo 25165824 > /proc/sys/net/core/optmem_max
+echo 8388608 1048576 16777216 > /proc/sys/net/ipv4/tcp_mem
+echo 786432 1048576 16777216 > /proc/sys/net/ipv4/udp_mem
+echo 8192 87380 16777216 > /proc/sys/net/ipv4/tcp_rmem
+echo 8192 87380 16777216 > /proc/sys/net/ipv4/tcp_wmem
+sysctl -w net.core.somaxconn=10000
+sysctl -w net.core.netdev_max_backlog=10000
+sysctl -w net.ipv4.ipfrag_high_thresh=50000000
+sysctl -w net.ipv6.ip6frag_high_thresh=50000000
+sysctl -w net.ipv4.ipfrag_time=2
+sysctl -w net.ipv6.ip6frag_time=2
+echo ff > /sys/class/net/eth0/queues/tx-0/xps_cpus
+echo ff > /sys/class/net/eth0/queues/rx-0/rps_cpus
+echo ff > /sys/class/net/eth1/queues/tx-0/xps_cpus
+echo ff > /sys/class/net/eth1/queues/rx-0/rps_cpus
+}
+
+# parameters:
 # $1: bridge name
 # $2: ip address
 setup_bridge()
@@ -12,7 +37,7 @@ setup_bridge()
   ifconfig $1 $2 up
 }
 
-# parameters: 
+# parameters:
 # $1: iface name
 # $2: bridge name
 setup_if()
@@ -48,12 +73,12 @@ setup_network()
 {
   setup_bridge $2 $3
   echo " setup-network-host bring up $2 successfully on soc: $1"
-  
+
   # setup interface and attach to bridge
   setup_if $4 $2
   echo " setup-network-host bring up $4 successfully on soc: $1"
   echo " setup-network-host add $4 into $2 successfully on soc: $1"
-  
+ 
   echo " setup-network-host add vlan $2.$5"
   add_vlan_to_if $2 $5 $6
 }
@@ -75,5 +100,10 @@ else
     setup_network $soc_num br0 192.168.1.2 eth0 4  192.168.4.2/24
     setup_network $soc_num br1 192.168.6.2 eth1 10 192.168.10.2/24
 fi
+
+
+#perf setting
+echo " setup-network-host perf setting "
+perf_set_if
 
 echo " setup-network-host end "
